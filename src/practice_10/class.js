8259530a4b8d05928f825b785a2a6b980f2b2fc0
Di Yuan <dyuan@thoughtworks.com>
2018-07-17 17:11:34 +0800

    [YUAN Di] finish practice10 - Rest

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,11 @@
+"use district";
 
+import BaseClass from "../practice_9/class.js";
+
+class Class extends BaseClass{
+    isIn(student){
+        return this === student.klass;
+    }
+}
+
+export default Class;
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,24 @@
+"use distirct";
 
+import Person from "../practice_10/person.js";
+
+class Teacher extends Person {
+    constructor(id, name, age, klasses){
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce(){
+        var className = `No Class`;
+        if(this.klasses && this.klasses.length > 0){
+            className = `Class ${this.klasses.map(klass => klass.number).join(", ")}`;
+        }
+        return super.introduce() + ` I am a Teacher. I teach ${className}.`;
+    }
+
+    isTeaching(student){
+        return this.klasses.includes(student.klass);
+    }
+}
+
+export default Teacher;
